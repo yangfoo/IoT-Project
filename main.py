174d0c973a176c8bdc5488d11b4b818f12c6ed66
jsoh0014 <62204580+yangfoo@users.noreply.github.com>
2022-10-12 16:54:25 +0800

    adding main

---
+++ main.py
@@ -0,0 +1,11 @@
+from LoginSystem.LoginSystem import LoginPage
+import tkinter as tk
+from tkinter import *
+import os
+import random
+import requests
+
+if __name__ == "__main__":
+    window = Tk()
+    LoginPage(window)
+    window.mainloop()
