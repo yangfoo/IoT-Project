1acc73576e7935711e059a14f0eaee5ddae4d0f9
jsoh0014 <62204580+yangfoo@users.noreply.github.com>
2022-10-13 10:44:54 +0800

    code for rfid

--- main.py
+++ main.py
@@ -4,13 +4,37 @@ from tkinter import *
 import os
 import random
 import requests
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+from time import sleep
 
 if __name__ == "__main__":
     RFID = True
     if RFID:
-        window = Tk()
-        LoginPageRIFD(window, 'jy')
-        window.mainloop()
+        relay = 24
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(relay,GPIO.IN)
+
+        # Create a object for the RFID module
+        read = SimpleMFRC522()
+        list_of_files = os.listdir()
+        while True:
+            print("Place your Tag")
+            id,Tag = read.read()
+            
+            id = str(id)
+                    
+            if id in list_of_files:
+                file1 = open(id,'r')
+                username = file1.read()
+                print("Welcome " + username)
+                window = Tk()
+                LoginPageRIFD(window, 'username')
+                window.mainloop()             
+            else:
+                print("Wrong Tag!")
+        
     else:
         window = Tk()
         Main(window)
