6c0d0d2125c15acb2be720b7fe394365b11dc77f
BondChia <kchi0047@student.monash.edu>
2022-10-13 17:52:34 +0800

    Encrypt Update

--- LoginSystem/LoginSystem.py
+++ LoginSystem/LoginSystem.py
@@ -168,19 +168,40 @@ class LoginPage:
         # return string
         return new
 
-    def CaesarEncryption(self, realText, step):
+    def CaesarEncryption(realText, step):
         outText = []
         cryptText = []
+        uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        symbol = ['~','!','@','#','$','%','^','&','*','(',')','-','_','=','+','{','}','[',']','|',':','/','?',',','.','<','>',' ']
         for eachLetter in realText:
-            if eachLetter in number:
+            if eachLetter in uppercase:
+                index = uppercase.index(eachLetter)
+                crypting = (index + step) % 26
+                cryptText.append(crypting)
+                newLetter = uppercase[crypting]
+                outText.append(newLetter)
+            elif eachLetter in lowercase:
+                index = lowercase.index(eachLetter)
+                crypting = (index + step) % 26
+                cryptText.append(crypting)
+                newLetter = lowercase[crypting]
+                outText.append(newLetter)
+            elif eachLetter in number:
                 index = number.index(eachLetter)
                 crypting = (index + step) % 10
                 cryptText.append(crypting)
                 newLetter = number[crypting]
-                outText.append(newLetter)  
-        return self.convert(outText)
-
+                outText.append(newLetter)
+            elif eachLetter in symbol:
+                index = symbol.index(eachLetter)
+                crypting = (index + step) % 28
+                cryptText.append(crypting)
+                newLetter = symbol[crypting]
+                outText.append(newLetter)	  
+        return convert(outText)
+        
     def MirrorEncryption(self, string):
         translated = '' #cipher text is stored in this variable
         i = len(string) - 1
@@ -389,6 +410,90 @@ def page():
     window = Tk()
     Main(window)
     window.mainloop()
+
+def convert(s):
+ 
+    # initialization of string to ""
+    new = ""
+    # traverse in the string
+    for x in s:
+        new += x
+    # return string
+    return new
+
+def CaesarEncryption(realText, step):
+	outText = []
+	cryptText = []
+	uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	symbol = ['~','!','@','#','$','%','^','&','*','(',')','-','_','=','+','{','}','[',']','|',':','/','?',',','.','<','>',' ']
+	for eachLetter in realText:
+		if eachLetter in uppercase:
+			index = uppercase.index(eachLetter)
+			crypting = (index + step) % 26
+			cryptText.append(crypting)
+			newLetter = uppercase[crypting]
+			outText.append(newLetter)
+		elif eachLetter in lowercase:
+			index = lowercase.index(eachLetter)
+			crypting = (index + step) % 26
+			cryptText.append(crypting)
+			newLetter = lowercase[crypting]
+			outText.append(newLetter)
+		elif eachLetter in number:
+			index = number.index(eachLetter)
+			crypting = (index + step) % 10
+			cryptText.append(crypting)
+			newLetter = number[crypting]
+			outText.append(newLetter)
+		elif eachLetter in symbol:
+			index = symbol.index(eachLetter)
+			crypting = (index + step) % 28
+			cryptText.append(crypting)
+			newLetter = symbol[crypting]
+			outText.append(newLetter)	  
+	return convert(outText)
+
+def CaesarDecryption(realText, step):
+	outText = []
+	cryptText = []
+	uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	symbol = ['~','!','@','#','$','%','^','&','*','(',')','-','_','=','+','{','}','[',']','|',':','/','?',',','.','<','>',' ']
+	for eachLetter in realText:
+		if eachLetter in uppercase:
+			index = uppercase.index(eachLetter)
+			crypting = (index - step) % 26
+			cryptText.append(crypting)
+			newLetter = uppercase[crypting]
+			outText.append(newLetter)
+		elif eachLetter in lowercase:
+			index = lowercase.index(eachLetter)
+			crypting = (index - step) % 26
+			cryptText.append(crypting)
+			newLetter = lowercase[crypting]
+			outText.append(newLetter)
+		elif eachLetter in number:
+			index = number.index(eachLetter)
+			crypting = (index - step) % 10
+			cryptText.append(crypting)
+			newLetter = number[crypting]
+			outText.append(newLetter)
+		elif eachLetter in symbol:
+			index = symbol.index(eachLetter)
+			crypting = (index - step) % 28
+			cryptText.append(crypting)
+			newLetter = symbol[crypting]
+			outText.append(newLetter)	
+	return convert(outText)
+	
+
+
+
+
+
 
 if __name__ == '__main__':
     page()
--- LoginSystem/encrypt_decrypt.py
+++ LoginSystem/encrypt_decrypt.py
@@ -18,15 +18,36 @@ def convert(s):
 def CaesarDecryption(realText, step):
 	outText = []
 	cryptText = []
+	uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	symbol = ['~','!','@','#','$','%','^','&','*','(',')','-','_','=','+','{','}','[',']','|',':','/','?',',','.','<','>',' ']
 	for eachLetter in realText:
-		if eachLetter in number:
+		if eachLetter in uppercase:
+			index = uppercase.index(eachLetter)
+			crypting = (index - step) % 26
+			cryptText.append(crypting)
+			newLetter = uppercase[crypting]
+			outText.append(newLetter)
+		elif eachLetter in lowercase:
+			index = lowercase.index(eachLetter)
+			crypting = (index - step) % 26
+			cryptText.append(crypting)
+			newLetter = lowercase[crypting]
+			outText.append(newLetter)
+		elif eachLetter in number:
 			index = number.index(eachLetter)
 			crypting = (index - step) % 10
 			cryptText.append(crypting)
 			newLetter = number[crypting]
 			outText.append(newLetter)
-	return convert(outText)    
+		elif eachLetter in symbol:
+			index = symbol.index(eachLetter)
+			crypting = (index - step) % 28
+			cryptText.append(crypting)
+			newLetter = symbol[crypting]
+			outText.append(newLetter)	
+	return convert(outText)
 
 def MirrorDecryption(string):
   translated = '' #cipher text is stored in this variable
